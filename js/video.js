9df84ab7a60c0bf367b303737cfacc26acdf1575
glni <glni@easj.dk>
2018-03-04 12:05:15 +0100

    Initial commit

---
+++ js/video.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    var current_time = 0;
+    var my_video = $("#about_video");
+    var c_welcome = $("#content_welcome");
+    var c_coffee = $("#content_coffee");
+    var c_baked = $("#content_baked_goods");
+    var c_promise = $("#content_promise");
+    var allAsides = $("aside");
+
+
+});
