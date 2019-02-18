7323feffcf7f1ed3d01380f9ac53e2f8e19e827a
glni <glni@easj.dk>
2019-02-18 17:59:44 +0100

    scss to css and ECMA6

--- js/video.js
+++ js/video.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
 
-    var current_time = 0;
-    var my_video = $("#about_video");
-    var c_welcome = $("#content_welcome");
-    var c_coffee = $("#content_coffee");
-    var c_baked = $("#content_baked_goods");
-    var c_promise = $("#content_promise");
-    var allAsides = $("aside");
+    let current_time = 0;
+    const $my_video = $("#about_video");
+    const $c_welcome = $("#content_welcome");
+    const $c_coffee = $("#content_coffee");
+    const $c_baked = $("#content_baked_goods");
+    const $c_promise = $("#content_promise");
+    const $allAsides = $("aside");
+
+    //Eventlistner til når videoen afspiller "timeupdate"
+
+        // [0] is selecting the DOM element instead of a jQuery Element
+
+        //current_time < 8.75
+
+        //current_time > 8.75
+
+        //current_time > 22.4
+
+        //current_time > 29.2
+
+        //current_time > 45
 
 
 });
